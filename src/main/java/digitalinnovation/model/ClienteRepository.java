0656e479115bd67fb46d3d1f6455886c40bb0b89
Soilenyis <soilenyis@gmail.com>
2023-08-31 10:18:49 -0300

    Padrões de Projetos com Spring Framework

---
+++ src/main/java/digitalinnovation/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package digitalinnovation.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
